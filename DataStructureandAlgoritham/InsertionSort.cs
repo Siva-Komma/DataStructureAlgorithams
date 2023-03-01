baee401d4ee9614959b0b42af2b2b91241db65b3
Siva-Komma <kommasivajyothi22@gmail.com>
2023-03-01 21:26:22 +0530

    Sorting Words using Insertion Sort

---
+++ DataStructureandAlgoritham/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureandAlgoritham
+{
+    class InsertionSort
+    {
+        public static void Sorting()
+        {
+            int i, j;
+            string temp=" " ;
+            string[] arr = { "Siva", "Komma", "Jyothi" };
+            for(i=1;i<arr.Length;i++)
+            {
+                for(j=i-1;j<arr.Length;j++)
+                {
+                    while(j>0 && arr[j].CompareTo(arr[j])>0)
+                    {
+                        temp = arr[i];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach(var ele in arr)
+            {
+                Console.WriteLine(ele) ;
+            }
+        }
+    }
+}
